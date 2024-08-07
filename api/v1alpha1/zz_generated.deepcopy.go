7a6318e3ec3d75cc4499ce10124e1e622404b32b
Baranitharan Chittharanjan <baranitharanece@gmail.com>
2024-08-08 11:34:49 +1200

    modified some logic

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -155,6 +155,11 @@ func (in *OsphealthcheckStatus) DeepCopyInto(out *OsphealthcheckStatus) {
 		in, out := &in.LastSuccessfulRunTime, &out.LastSuccessfulRunTime
 		*out = (*in).DeepCopy()
 	}
+	if in.Healthy != nil {
+		in, out := &in.Healthy, &out.Healthy
+		*out = new(bool)
+		**out = **in
+	}
 	if in.ExternalNotifiedTime != nil {
 		in, out := &in.ExternalNotifiedTime, &out.ExternalNotifiedTime
 		*out = (*in).DeepCopy()
