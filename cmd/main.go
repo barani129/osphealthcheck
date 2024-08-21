a0fc09caeba1e29f4ab12c0bef4ecae8a661acf4
Baranitharan Chittharanjan <baranitharanece@gmail.com>
2024-08-21 18:59:35 +1200

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
@@ -28,9 +29,12 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -108,6 +112,8 @@ func main() {
 		tlsOpts = append(tlsOpts, disableHTTP2)
 	}
 
+	httpClient := http.Client{}
+
 	// webhookServer := webhook.NewServer(webhook.Options{
 	// 	TLSOpts: tlsOpts,
 	// })
@@ -139,9 +145,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	gvk := schema.GroupVersionKind{
+		Group:   "",
+		Version: "v1",
+		Kind:    "Pod",
+	}
+
+	restClient, err := apiutil.RESTClientForGVK(gvk, false, mgr.GetConfig(), serializer.NewCodecFactory(mgr.GetScheme()), &httpClient)
+	if err != nil {
+		setupLog.Error(err, "unable to create REST client")
+	}
+
 	if err = (&controller.OsphealthcheckReconciler{
 		Kind:                     "Osphealthcheck",
+		RESTClient:               restClient,
 		Client:                   mgr.GetClient(),
+		RESTConfig:               mgr.GetConfig(),
 		Scheme:                   mgr.GetScheme(),
 		ClusterResourceNamespace: clusterResourceNamespace,
 	}).SetupWithManager(mgr); err != nil {
